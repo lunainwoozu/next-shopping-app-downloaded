2ea796963f3cb86752556f755f23c623695212b8
run2u7@gmail.com <run2u7@gmail.com>
2026-05-29 11:12:33 +0900

    style: 폰트 적용 방법 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
+import "pretendard/dist/web/variable/pretendardvariable.css";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { QueryProvider } from "@/providers/QueryProvider"; // ← 추가
